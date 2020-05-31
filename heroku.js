5fce8943bdc1ac7eefdf4533a61cef5108af0805
Yuaru-Imd <xfxixz@gmail.com>
2020-06-01 00:13:48 +0800

    repair

--- heroku.js
+++ heroku.js
@@ -32,13 +32,7 @@ bot.on('message', (msg) => {
     
     if (msg.text.toLowerCase().indexOf(Hi) === 0) {
         bot.sendMessage(msg.chat.id,"Hello "+msg.from.first_name);
-
-        bot.sendPhoto({
-          chat_id = chat_id,
-          photo = 'https://www.sciencemag.org/sites/default/files/styles/article_main_image_-_1280w__no_aspect_/public/dogs_1280p_0.jpg?itok=6jQzdNB8',
-          caption = "Telegram Logo"
-        });
-        
+       
       }
         if (msg.text.toLowerCase().indexOf(Bye) === 0) {
           bot.sendMessage(msg.chat.id,"Bye "+ msg.from.first_name);
