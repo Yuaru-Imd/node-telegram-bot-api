935fcb0d86abe5876f4aa6497fec62e368d75b9c
Yuaru-Imd <xfxixz@gmail.com>
2020-06-01 00:50:03 +0800

    Update heroku.js

--- heroku.js
+++ heroku.js
@@ -32,16 +32,12 @@ bot.on('message', (msg) => {
 
 
     if (msg.text.toLowerCase().indexOf(Hi) === 0) {
-        const starter = new TelegrafInlineMenu(ctx => `Hey ${ctx.from.first_name}!`);
-        starter.setCommand('start');
         bot.sendMessage(msg.chat.id,"Hello "+msg.from.first_name);
         bot.sendPhoto(chatID,photo, { caption: "People Die If They Being Killed"});
-        starter.simpleButton('I am excited!', 'a', {
+        bot.simpleButton('I am excited!', 'a', {
           doFunc: ctx => ctx.reply('As am I!')
         });
-        const bot = new Telegraf(process.env.BOT_TOKEN);
-        bot.use(menu.init());
-        bot.startPolling();
+
        
       }
         if (msg.text.toLowerCase().indexOf(Bye) === 0) {
