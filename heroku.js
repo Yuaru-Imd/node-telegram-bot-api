915f07c8b78fe7b14ed132bb87d336525a6a7ec3
Yuaru-Imd <xfxixz@gmail.com>
2020-06-01 13:56:12 +0800

    Update heroku.js

--- heroku.js
+++ heroku.js
@@ -5,7 +5,7 @@
 
 require('dotenv/config');
 const Telegraf = require('telegraf');
-const TelegrafInlineMenu = require('../dist/source');
+const TelegrafInlineMenu = require('telegraf-inline-menu');
 const TOKEN = process.env.TELEGRAM_TOKEN || 'YOUR_TELEGRAM_BOT_TOKEN';
 const TelegramBot = require('node-telegram-bot-api');
 const options = {
@@ -28,41 +28,33 @@ bot.on('message', (msg) => {
   var Bye = "bye";
   const chatID = msg.chat.id;
   const photo = 'https://grandorder.wiki/images/thumb/3/37/Fgo-mainpage-logo.png/300px-Fgo-mainpage-logo.png';
-  const menu = new TelegrafInlineMenu('Main Menu');
-  menu.urlButton('EdJoPaTo.de', 'https://edjopato.de');
-  let mainMenuToggle = false
-  menu.toggle('toggle me', 'a', {
-    setFunc: (_ctx, newState) => {
-      mainMenuToggle = newState
-    },
-    isSetFunc: () => mainMenuToggle
-  });
-  menu.simpleButton('click me', 'c', {
-    doFunc: async ctx => ctx.answerCbQuery('you clicked me!'),
-    hide: () => mainMenuToggle
-  });
-  
-  menu.simpleButton('click me harder', 'd', {
-    doFunc: async ctx => ctx.answerCbQuery('you can do better!'),
-    joinLastRow: true,
-    hide: () => mainMenuToggle
-  });
-  
-  let selectedKey = 'b'
-  menu.select('s', ['A', 'B', 'C'], {
-    setFunc: async (ctx, key) => {
-      selectedKey = key
-      await ctx.answerCbQuery(`you selected ${key}`)
-    },
-    isSetFunc: (_ctx, key) => key === selectedKey
-  });
-  
- 
+   
+   
     
   if (msg.text.toLowerCase().indexOf(Hi) === 0) {
       bot.sendMessage(msg.chat.id,"Hello "+msg.from.first_name);
       bot.sendPhoto(chatID,photo, { caption: "People Die If They Being Killed"});
+      const opts = {
+        reply_markup: {
+            inline_keyboard: [
+                [{
+                        text: 'BTC',
+                        callback_data: JSON.stringify({
+                            command: 'price',
+                            'base': 'BTC'
+                        })
+                    },{
+                      text: 'EUR',
+                      callback_data: JSON.stringify({
+                          command: 'price',
+                          'base': 'EUR'
+                      })
+                  }
+                ]
+              ]
+            }       
       
+    };
     }
       if (msg.text.toLowerCase().indexOf(Bye) === 0) {
         bot.sendMessage(msg.chat.id,"Bye "+ msg.from.first_name);
